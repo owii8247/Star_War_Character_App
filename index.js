8138c9b8d7e7e899c8c16d5e38f41a4e7e4c79c3
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2022-05-28 15:18:35 +0530

    appended the data working on styling

---
+++ index.js
@@ -0,0 +1,52 @@
+
+let search_div = document.getElementById("character")
+
+async function search(q){
+    try{
+        let url =`https://swapi.dev/api/people/?search=${q}`
+
+        let res = await fetch(url)
+
+        let data = await res.json()
+
+        return data.results 
+
+    }
+    catch (err)
+    {
+        console.log("err :",err)
+    }
+}
+
+function appendCharacter(character) {
+    search_div.innerHTML = null;
+    if (character === undefined) {
+        return false
+    }
+    character.forEach(function (el) {         
+        let name = document.createElement("p")
+        name.innerText = el.name
+
+        let dob = document.createElement("p")
+        dob.innerText = el.birth_year
+        dob.style.fontSize ="x-small"
+
+        let gender = document.createElement("p")
+        gender.innerText = el.gender
+
+        search_div.append(name,dob,gender)
+    })
+}
+
+async function main() {
+    let query = document.getElementById("query").value;
+    //console.log("query:",query)
+
+    let response = search(query)     
+
+    let data = await response
+    console.log("data:",data)
+
+    appendCharacter(data)
+
+}
